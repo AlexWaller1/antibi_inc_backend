1784e522250c533e9a8b805bc17218189b5cd2f8
Alex Waller <alex@artofalexwaller.com>
2021-03-15 14:00:19 -0400

    ActiveRecord Migrations

---
+++ db/migrate/20210315175345_create_hybrids.rb
@@ -0,0 +1,13 @@
+class CreateHybrids < ActiveRecord::Migration[6.1]
+  def change
+    create_table :hybrids do |t|
+      t.string :name
+      t.string :species
+      t.string :personality
+      t.text :biography
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
