0ace5b8211be05d4aac73a5bb7b1e623159f50dc
Alex Waller <alex@artofalexwaller.com>
2021-03-15 13:36:33 -0400

    controller actions

--- app/controllers/api/v1/hybrids_controller.rb
+++ app/controllers/api/v1/hybrids_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::HybridsController < ApplicationController
 
     def index
         @hybrids = Hybrids.all
-        render json: @hybrids
+        render json: @hybrids, status: 200
+    end
+
+    def show
+        @hybrid = Hybrid.find(params[:id])
+        render json: @hybrid, status: 200
     end
 
 end
